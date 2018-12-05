d946e0412cf86ccc8620c97a65a10256932f24aa
Sophie Graham <sophiergraham1@gmail.com>
2018-12-05 09:44:04 -0800

    finished client polls container

--- client/src/containers/PollsContainer.js
+++ client/src/containers/PollsContainer.js
@@ -1,1 +1,21 @@
 import { connect } from 'react-redux';
+import { getPolls } from '../selectors/polls';
+import { fetchPolls } from '../actions/polls';
+import withList from '../components/withList';
+import Poll from '../components/polls/Poll';
+import withFetch from '../components/withFetch';
+
+const mapStateToProps = state => ({
+  list: getPolls(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch: () => dispatch(fetchPolls())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withFetch(
+  withList(Poll)
+));
